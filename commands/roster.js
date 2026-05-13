80d669ae06cb191c091e1dfcd78c159721b930a8
puffindragonsguild <puffindragonsguild@gmail.com>
2026-05-13 23:03:21 +0100

    Delete commands/roster.js

--- commands/roster.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'roster',
-    description: 'Displays the current raid roster.',
-    adminOnly: false,
-    execute(message, args, client, db, raidManager) {
-        raidManager.displayRoster(message.channel);
-    },
-};
