9a8845ec2038ac98878c7d9ff1d7c6fcc62bf33a
puffindragonsguild <puffindragonsguild@gmail.com>
2026-05-13 23:02:27 +0100

    Delete commands/hail.js

--- commands/hail.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'hail',
-    description: 'Praise the Queen!',
-    adminOnly: false, // Anyone can use this
-    execute(message, args) {
-        message.reply('HAIL FORTUNA FELIS! 👑');
-    },
-};
