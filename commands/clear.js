1a7c74abda6d8113c5d6160af1f6b15006a6d2de
puffindragonsguild <puffindragonsguild@gmail.com>
2026-05-13 23:02:09 +0100

    Delete commands/clear.js

--- commands/clear.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'clear',
-    description: 'Wipes the raid roster clean.',
-    adminOnly: true,
-    execute(message, args, client, db) {
-        db.prepare('DELETE FROM signups').run();
-        message.reply('🧹 **Roster wiped clean!**');
-    },
-};
