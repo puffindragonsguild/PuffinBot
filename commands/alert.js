bb12785b80e4a6f2b46a6c759b1ebab7693ad1d9
puffindragonsguild <puffindragonsguild@gmail.com>
2026-05-21 09:23:48 +0100

    Update alert.js

--- commands/alert.js
+++ commands/alert.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, MessageFlags } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
